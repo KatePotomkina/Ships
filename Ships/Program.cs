3fb814fce30431458cd4d68261f8529d65044f43
kate.potomkina <kate.potomkina@gmail.com>
2023-10-06 19:03:44 +0300

    Fix bugs

--- Ships/Program.cs
+++ Ships/Program.cs
@@ -20,32 +20,41 @@ namespace Ships
 
         private static void Main(string[] args)
         {
+            //Calculate all one line ships (include double line(2ships))
             var count = 0;
-            var shipLength = 0;
             for (var row = 0; row < 10; row++)
-            {
-                for (var col = 0; col < 10; col++)
+            for (var col = 0; col < 10; col++)
+                if (ships[row, col] == 1)
                 {
-                    if (ships[row, col] == 1)
-                    {
-                        count++;
-                     ships[row, col] = 0;
-                        // Check to the right 
-                        for (var i = col + 1; i < 10 && ships[row, i] == 1; i++)
-                        {
-                            ships[row, i] = 0;
-                        }
-                        // Check below 
-                        for (var j = row + 1; j < 10 && ships[j, col] == 1; j++)
-                        {
-                            ships[j, col] = 0;
-                        }
-                    }
+                    count++;
+                    ships[row, col] = 0;
+                    calculateDoubleLineShips(row, col);
+                    // Check to the right 
+                    for (var i = col + 1;
+                         i < 10 && ships[row, i] == 1;
+                         i++)
+                        ships[row, i] = 0;
+
+                    // Check below 
+                    for (var j = row + 1;
+                         j < 10 && ships[j, col] == 1;
+                         j++)
+                        ships[j, col] = 0;
                 }
-            }
+
             Console.WriteLine(count);
-                    Console.ReadLine();
-                }
-            }
+            Console.ReadLine();
         }
-   
+
+        private static void calculateDoubleLineShips(int row, int col)
+        {
+            if (row < 9 && col < 9)
+                if (ships[row + 1, col] == 1 &&
+                    ships[row, col + 1] == 1 &&
+                    ships[row + 1, col + 1] == 1)
+                    for (var i = row; i <= row + 1; i++)
+                    for (var j = col; j <= col + 1; j++)
+                        ships[i, j] = 0;
+        }
+    }
+}
